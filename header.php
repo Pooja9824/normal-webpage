e8a4a68c4cac9e02ea597e7670418a8a32a0996c
Jap <jeet63240@gmail.com>
2021-12-04 09:26:39 +0530

    Set proper files and their paths also moved static pages to proper php pages.

---
+++ header.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>Welcome || MYPAY</title>
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <h1 class="">My pay</h1>
+            </div>
+            <div class="menu">
+                <a href="my pay 2.html">Home</a>
+                <a href="My pay body.html">Login</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </div>
+        </nav>
+    </header>
