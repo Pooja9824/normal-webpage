71ce4b00c49a4576ed134820fb1aeec98bcc8df6
Jap <jeet63240@gmail.com>
2021-12-05 09:12:28 +0530

    Add Footer content

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+<div class="footer">
+    <div class="dev">
+        @Pooja
+    </div>
+    <div class="social">
+        <a href="#">Instagram</a>
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">GitHub</a>
+    </div>
+</div>
+
+
 </body>
 
 </html>
