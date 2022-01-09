f59dd6426f3f3f375ed77cc4cb8d430e8bf3ee20
Jap <jeet63240@gmail.com>
2022-01-09 17:31:14 +0530

    Work started for making page responsive.

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <title>Welcome || MYPAY</title>
     <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
 </head>
 
 <body>
@@ -11,11 +12,12 @@
             <div class="logo">
                 <h1 class="">My pay</h1>
             </div>
-            <div class="menu">
+            <div class="menu" id="topnav">
                 <a href="index.php">Home</a>
                 <a href="login.php">Login</a>
                 <a href="#">About</a>
                 <a href="#">Contact</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()"><img src="images/menu.png" alt="menu" height="100px" width="100px"></a>
             </div>
         </nav>
     </header>
